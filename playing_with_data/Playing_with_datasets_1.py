ac5cb3956cb6da76cdf366f21e83dfe77992a688
EndOfTheGlory <52138220+EndOfTheGlory@users.noreply.github.com>
2021-05-12 09:37:08 +0300

    All notebooks

---
+++ playing_with_data/Playing_with_datasets_1.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 31 15:35:13 2020
+
+@author: EndOfTheGlory
+"""
+
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+data = sklearn.data.load_iris
